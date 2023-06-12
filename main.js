f6170e4371313b9785b869ce92df0ab0dab313ac
andrewsegovia00 <andrewsegovia4@gmail.com>
2023-06-12 08:59:52 -0700

    Added functionality to the restart button Added message header to keep up with turns Added winner and tie message Added board color logic Added turn logic

--- main.js
+++ main.js
@@ -1,123 +1,153 @@
 //Constants / Global variables
 
-//Player 1 Object
-const player1 = {
-    name: '',
-    turn: false,
-    win: false
-}
-
-//Player 2 Object
-const player2 = {
-    name: '',
-    turn: false,
-    win: false
-}
-
-let currentPlayer = 1;
-
-let boardState = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-// Array of winning combinations
-const winningCombinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-    [0, 4, 8], [2, 4, 6] // Diagonals
-  ];
 
 //Object that contains function that start the game
 const startGame = {
     init: init,
     playerNames: getPlayerNames,
     hidePlayer: hidePlayerForm,
-    addPlayer: addPlayerForm,
     hideBoard: hideBoard,
     addBoard: addBoard,
-    playTurn: playTurn,
+    setPlayerNames: setPlayerNames,
+    initializeGame: initializeGame,
+    renderBoard: renderBoard,
+
 
 }
+
+//Board colors
+const colors = {
+    null: 'black',
+    '1': 'grey', 
+    '-1': 'purple'
+};
+
+// Variables tracking state
+let board = [null, null, null, null, null, null, null, null, null];
+let turn = 1;
+let winner = null; 
+
+// Store elements on the page that will be accessed in code more than once in variables:
+const squares = document.querySelectorAll('#board div');
+
+
+//Player 1 Object
+const player1 = {
+    name: '',
+}
+
+//Player 2 Object
+const player2 = {
+    name: '',
+}
+
+// Array of winning combinations
+const winningCombinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6] // Diagonals
+];
+
+function initializeGame() {
+    board = [null, null, null, null, null, null, null, null, null];
+    turn = 1;
+    winner = null;
+}
+
 function renderBoard() {
-    const board = document.getElementById('board');
-    for (let i = 0; i < boardState.length; i++) {
-      const square = document.createElement('div');
-      square.id = `c${i}r${Math.floor(i / 3)}`;
-      square.classList.add('cell');
-      square.addEventListener('click', handleSquareClick);
-      board.appendChild(square);
+    squares.forEach((square, index) => {
+    square.style.backgroundColor = colors[board[index]];
+    });  
+
+//Need to hide and unhide once message pops up
+    const message = document.getElementById('message');
+
+    if (winner !== null) {
+      if (winner === 'T') {
+        message.textContent = 'It\'s a tie!';
+      } else {
+        const player = winner === 1 ? 'X' : 'O';
+        message.textContent = `Player ${player} wins!`;
+      }
+    } else {
+      const player = turn === 1 ? 'X' : 'O';
+      message.textContent = `Current turn: Player ${player}`;
     }
-  }
+}
+
+function checkWin() {
+    for (let i = 0; i < winningCombinations.length; i++) 
+    {
+        const [comb1, comb2, comb3] = winningCombinations[i];
+        if (board[comb1] !== null && board[comb1] === board[comb2] && board[comb2] === board[comb3]) 
+        {
+            winner = board[comb1];
+            renderBoard();
+            break;
+        }
+    }
+}  
+
+function checkTie() {
+    if (!board.includes(null) && winner === null) 
+    {
+      winner = 'T';
+      renderBoard();
+    }
+}
+
+function handleSquareClick(event) {
+    const squareIndex = parseInt(event.target.dataset.index);
+  
+    if (board[squareIndex] !== null || winner !== null) {
+      return; // Square is already taken or game is over
+    }
+    board[squareIndex] = turn;
+    renderBoard();
+  
+    checkWin();
+    checkTie();
+  
+    // Toggle the turn
+    turn *= -1;
+    
+}
 
 function squareClick(event) {
     const squareIndex = parseInt(event.target.dataset.index);
-    if (boardState[squareIndex] === 0 && currentPlayer === 1) 
+    if (board[squareIndex] === 0 && currentPlayer === 1) 
     {
-        boardState[squareIndex] = 1;
+        board[squareIndex] = 1;
         event.target.textContent = 'X';
-    //     if (checkWin(1)) 
-    //     {
-    //         console.log('Player 1 wins!');
-    //     } 
-    //     else if (boardState.indexOf(0) === -1) 
-    //     {
-    //         console.log('It\'s a draw!');
-    //     } 
-    //     else 
-    //     {
-    //         currentPlayer = 2;
-    //     }
-    // } 
-    // else if (boardState[squareIndex] === 0 && currentPlayer === 2) 
-    // {
-    //     boardState[squareIndex] = 2;
-    //     event.target.textContent = 'O';
-    //     if (checkWin(2)) 
-    // {
-    //     console.log('Player 2 wins!');
-    // } 
-    // else if (boardState.indexOf(0) === -1) 
-    // {
-    //     console.log('It\'s a draw!');
-    // } 
-    // else 
-    // {
-    //     currentPlayer = 1;
-    // }
     }
 }
 
-function playTurn(squareIndex, player) {
-    // Update the board state with the current player's symbol (1 for X, 2 for O)
-    boardState[squareIndex] = player;
-  
-    // Set the content of the square to 'X' or 'O'
-    const square = document.querySelector(`#c${squareIndex}`);
-    square.textContent = player === 1 ? 'X' : 'O';
-  
-    // Check if the current player has won
-    if (checkWin(player)) {
-      // Current player wins
-      console.log(`Player ${player} wins!`);
-    } else if (boardState.indexOf(0) === -1) {
-      // The board is full, it's a draw
-      console.log('It\'s a draw!');
-    } else {
-      // Toggle the current player's turn
-      currentPlayer = currentPlayer === 1 ? 2 : 1;
-    }
-  }
-  
+function setPlayerNames() {
+    //Obtains the h1 elements for the names
+    const headerName1 = document.querySelector('#displayName');
+    const headerName2 = document.querySelector('#displayName2');
 
-function checkWin(player) {
-    for (let i = 0; i < winningCombinations.length; i++) 
-    {
-        const [a, b, c] = winningCombinations[i];
-        if (boardState[a] === player && boardState[b] === player && boardState[c] === player) 
-        {
-            return true;
-        }
-    }
-    return false;
-}     
+    //Removes hidden, adds a class
+    headerName1.classList.remove('playerName');
+    headerName1.classList.add('hidden');
+
+    //Removes hidden, adds a class
+    headerName2.classList.remove('playerName');
+    headerName2.classList.add('hidden');
+
+    //Hides the Form
+    const playerForm = document.querySelector('#playerForm');
+    playerForm.classList.remove('hidden');
+    playerForm.classList.add('playerForm');
+
+    const restBtn = document.querySelector('#restart');
+    restBtn.classList.remove('btns');
+    restBtn.classList.add('hidden');
+
+    const message = document.getElementById('message');
+    message.classList.remove('something');
+    message.classList.add('hidden');
+}
 
 //Hide Player Form
 function hidePlayerForm() {
@@ -143,10 +173,6 @@ function hidePlayerForm() {
     playerForm.classList.add('hidden');
 }
 
-function addPlayerForm() {
-    console.log('hi');
-}
-
 //Obtains names from the input values
 function getPlayerNames(event) {
     //Prevents parent event handler
@@ -165,6 +191,7 @@ function getPlayerNames(event) {
     //Hides Form and adds names to h1 player elements
     startGame.hidePlayer();
     startGame.addBoard();
+
 }
 
 //Hides the boardgame while the user inputs information
@@ -193,20 +220,32 @@ function addBoard() {
         e.classList.remove('hidden');
         e.classList.add('cell');
     })
+
+    const restBtn = document.getElementById('restart');
+    restBtn.classList.remove('hidden');
+    restBtn.classList.add('btns');
 }
 
 
 //Definition of the Init function that starts the game
 function init() {
-    //Add the initializers like addPlayerform
-    //Reset the Board
-    //This will be called by the reset button and when the game starts up
-    //
+    startGame.hideBoard();
+    startGame.initializeGame();
+    startGame.renderBoard();
     
 }
 
-//Starts the game
-// startGame.init;
+const replayButton = document.getElementById('restart');
+replayButton.addEventListener('click', () => {
+  startGame.initializeGame();
+  startGame.renderBoard();
+  startGame.hideBoard();
+  startGame.setPlayerNames();
+});
+
+initializeGame();
+renderBoard();
+
 
 //Adds functionality to the Player 1 & Player 2 Form
 const startBtn = document.getElementById('startGameBtn');
@@ -214,8 +253,6 @@ startBtn.addEventListener('click', startGame.playerNames);
 
 startGame.hideBoard();
 
-const squares = document.querySelectorAll('#board div');
-squares.forEach((square, index) => {
-  square.addEventListener('click', squareClick);
-  square.setAttribute('data-index', index);
-});
+squares.forEach((square) => {
+    square.addEventListener('click', handleSquareClick);
+  });
